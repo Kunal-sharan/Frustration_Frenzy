68fa59067fa5b791da66306bbb63977cfab2d614
Kunal sharan <codedragonks07@gmail.com>
2023-11-21 04:00:43 +0530

    landing_pagecommit01

--- app/Components/MainPage.jsx
+++ app/Components/MainPage.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
-
+import Image from "next/image";
+import React from "react";
+import bg from "./bg01.png";
+import bgc1 from "./bg-c1.png";
 const MainPage = () => {
   return (
-    <div className='w-1/2 flex-auto flex-col m-8 p-4 justify-items-center'>
-        <div className='h-100 w-100 font-bold text-6xl mb-4 leading-normal'>
-            The Best Rageroom Service in India
+    <div className="w-full flex-col">
+      <div className="w-full flex place-content-center ">
+        <div className=" w-full h-[79vh] flex place-content-start relative">
+          <div
+            className="w-full h-[95vh] bg-contain bg-no-repeat"
+            style={{ backgroundImage: "url('./bg01.jpg')" }}
+          >
+            <Image src={bg} className="w-full h-5/6" />
+          </div>
+          <div className="w-2/5 h-full absolute bg-white/20 backdrop-blur-lg grid place-content-start  ">
+            <div className="rounded-3xl grid place-content-center">
+              <Image
+                src={bgc1}
+                className="w-3/4 h-full  drop-shadow-xl  rounded-3xl"
+              />
+            </div>
+            <div className="m-5">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro ad
+              sed aliquam maxime provident eum, debitis sunt corporis molestiae
+              consequatur alias aliquid distinctio ullam asperiores nostrum
+              dolor adipisci voluptates reiciendis.
+            </div>
+          </div>
         </div>
-        <div className='font-medium text-justify mb-6 text-xl w-1/2 leading-normal'>
-            Save Time and Money with Online Booking and Flat Rate Pricing!
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl mt-5">
+        <figure>
+          <img
+            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-black">Shoes!</h2>
+          <p className=" text-black">
+            If a dog chews shoes whose shoes does he choose?
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
-        <button className='font-bold px-10 py-3 bg-green-800 shadow-md rounded-md hover:bg-green-400'>
-            Book Now
-        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;

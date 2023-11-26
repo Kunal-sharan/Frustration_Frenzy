f22c0527ab63565da923b3333ab76ede1f84ece6
Kunal sharan <codedragonks07@gmail.com>
2023-11-27 03:17:34 +0530

    update

--- app/sendEmail/page.jsx
+++ app/sendEmail/page.jsx
@@ -19,33 +19,47 @@ export default function EmailForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-1 p-8">
-      <label className="text-black">email</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        className="bg-white-green text-black"
-      />
-      <label className="text-black">subject</label>
-      <input
-        type="text"
-        value={subject}
-        onChange={(e) => setSubject(e.target.value)}
-        required
-        className="bg-white-green text-black"
-      />
-      <label className="text-black">message</label>
-      <textarea
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        required
-        className="bg-white-green text-black"
-      />
-      <button type="submit" className="btn-primary">
-        Send Email
-      </button>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-1 p-8 bg-dark-green w-1/2 m-auto rounded-3xl my-10"
+    >
+      <div className="flex flex-col gap-1 p-8 place-content-center">
+        <label className="text-white-green font-semibold text-xl">Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className=" text-black rounded-md p-1"
+        />
+        <label className="text-white-green font-semibold text-xl">
+          subject
+        </label>
+        <input
+          type="text"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+          required
+          className=" text-black rounded-md p-1"
+        />
+        <label className="text-white-green font-semibold text-xl">
+          message
+        </label>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+          className=" text-black rounded-md p-1"
+        />
+        <div className="my-4">
+          <button
+            type="submit"
+            className="btn btn-primary  bg-sec-red text-white-green w-60 hover:bg-primary-green"
+          >
+            Send Email
+          </button>
+        </div>
+      </div>
     </form>
   );
 }

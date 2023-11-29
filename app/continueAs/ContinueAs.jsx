68ad734f788b4d198ddfbd32e7264b3f2bc843e8
Kunal sharan <codedragonks07@gmail.com>
2023-11-29 20:22:05 +0530

    updateed

--- app/continueAs/ContinueAs.jsx
+++ app/continueAs/ContinueAs.jsx
@@ -1,22 +1,29 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import VideoLeft from "../Components/VideoLeft";
+import homeVideo from "../../public/home_video.mp4"
 
 const ContinueAs = () => {
   return (
-    <div className="flex flex-col gap-10 items-center bg-dark-green w-1/2 mx-auto my-auto rounded-2xl p-20 mt-40 h-96">
-      <h1 className="font-bold text-3xl font-sans text-white-green">
-        Continue As...
-      </h1>
-      <button className="btn btn-primary bg-primary-green w-60 text-white-green hover:bg-black border-2 border-white hover:border-white">
-        <Link href="/DoctorForm">Doctor</Link>
-      </button>
-      <Link href="/mainpage">
-        <button className="btn btn-primary bg-sec-red w-60 text-white-green hover:bg-black border-2 border-white hover:border-white">
-          Client
-        </button>
-      </Link>
+    <><div className="w-full h-full ">
+      <VideoLeft url={homeVideo} />
     </div>
+    
+    <div className="flex flex-col absolute left-0 right-0 opacity-95 gap-10 items-center bg-white-green w-1/2 m-auto  p-20 mt-40 h-96 z-50 border-solid border-white border-8">
+
+        <h1 className="font-bold text-4xl font-sans text-black">
+          Continue As...
+        </h1>
+        <button className="btn btn-primary bg-[#618264] w-60 text-xl text-black hover:bg-white hover:drop-shadow-xl border-2 border-white hover:border-white">
+          <Link href="/DoctorForm">Doctor</Link>
+        </button>
+        <Link href="/mainpage">
+          <button className="btn btn-primary bg-[#618264] w-60 text-xl text-black hover:bg-white hover:drop-shadow-xl border-2 border-white hover:border-white">
+            Client
+          </button>
+        </Link>
+      </div></>
   );
 };
 
--- app/layout.js
+++ app/layout.js
@@ -12,13 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <Navbar />
           <div className="overflow-hidden">{children}</div>
         </body>
       </html>
-    </ClerkProvider>
   );
 }
